bff53f3c171d55c5795b9c09edae12be0fab1a60
Alex Mleczko <alex.mleczko@arc-consulting.pl>
2024-12-03 15:25:07 +0100

    User repository - add user

---
+++ DataAccess/Repositories/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using DataAccess.Models;
+using DataAccess.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public class UserRepository : BaseRepository, IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public UserRepository(ApplicationDbContext context) :base(context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateUser(UserModel user)
+        {
+            _context.Users.Add(user);
+            await SaveAsync();
+        }
+    }
+}
